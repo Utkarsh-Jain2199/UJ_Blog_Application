1f0d691c9edc55a97f0fcfbc25d407750975840d
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-08-21 00:27:13 +0530

    Update BlogApplication.java

--- src/main/java/com/mountblue/blogapplication/BlogApplication.java
+++ src/main/java/com/mountblue/blogapplication/BlogApplication.java
@@ -1,13 +1,21 @@
 package com.mountblue.blogapplication;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
+    }
+    @PostConstruct
+    public void init(){
+        // Setting Spring Boot default time zone to IST (Indian Standard Time)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
     }
 
 }
